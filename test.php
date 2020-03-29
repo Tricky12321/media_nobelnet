a653de647ebd530c17d40954468bd137a9815be7
tricky12321 <thue.v.iversen@gmail.com>
2020-03-29 11:11:58 +0200

    Test

--- test.php
+++ test.php
@@ -4,6 +4,8 @@
 $volume = exec("mediactl receiver get volume");
 var_dump($volume);
 */
+$var1 = exec("mediactl receiver get volume");
 
 $var = intval(((string) exec("mediactl receiver get volume"))[0]);
 var_dump($var);
+var_dump($var1);
